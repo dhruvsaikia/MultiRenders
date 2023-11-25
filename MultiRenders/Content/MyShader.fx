6e297554c919ce0a61f82ea4a905565a8865b170
dhruvsaikia <85579996+dhruvsaikia@users.noreply.github.com>
2023-11-24 21:15:18 -0500

    new committ

--- MultiRenders/Content/MyShader.fx
+++ MultiRenders/Content/MyShader.fx
@@ -1,46 +1,82 @@
 ﻿#if OPENGL
-	#define SV_POSITION POSITION
-	#define VS_SHADERMODEL vs_3_0
-	#define PS_SHADERMODEL ps_3_0
+#define SV_POSITION POSITION
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
 #else
-	#define VS_SHADERMODEL vs_4_0_level_9_1
-	#define PS_SHADERMODEL ps_4_0_level_9_1
+#define VS_SHADERMODEL vs_4_0_level_9_1
+#define PS_SHADERMODEL ps_4_0_level_9_1
 #endif
 
 matrix WorldViewProjection;
+float3 CameraPosition;
+matrix World;
+matrix ViewProjection;
+float3 LightDirection;
+float3 LightColor;
+float AmbientStrength;
+float3 WorldMin;
+float3 WorldMax;
+float3 DiffuseColor = float3(0, 1, 0);
+float4 SpecularColor = float4(1, 0, 0, 1); 
+float SpecularPower = 50.0f; 
 
+
+texture teapotTexture;
+sampler BasicTextureSampler = sampler_state
+{
+
+    texture = <teapotTexture>;
+   
+};
 struct VertexShaderInput
 {
-	float4 Position : POSITION0;
-	float4 Color : COLOR0;
+    float4 Position : POSITION0;
+    float3 Normal : NORMAL;
+    float2 UV : TEXCOORD0;
 };
 
 struct VertexShaderOutput
 {
-	float4 Position : SV_POSITION;
-	float4 Color : COLOR0;
+    float4 Position : SV_POSITION;
+    float3 WorldPosition : TEXCOORD1;
+    float3 Normal : NORMAL;
+    float2 UV : TEXCOORD0;
 };
 
 VertexShaderOutput MainVS(in VertexShaderInput input)
 {
-	VertexShaderOutput output = (VertexShaderOutput)0;
+    VertexShaderOutput output = (VertexShaderOutput) 0;
 
-	output.Position = mul(input.Position, WorldViewProjection);
-	output.Color = input.Color;
+    output.Position = mul(input.Position, WorldViewProjection);
+    output.UV = input.UV;
+    output.WorldPosition = mul(input.Position, World).xyz;
 
-	return output;
+    return output;
 }
 
-float4 MainPS(VertexShaderOutput input) : COLOR
+float4 MainPS(VertexShaderOutput input) : SV_Target
 {
-	return input.Color;
+ 
+    float3 worldPosition = input.WorldPosition;
+    float3 basecolor = tex2D(BasicTextureSampler, input.UV).rgb;
+    
+    //Tint base on World position
+    float3 tint = float3(worldPosition.x, worldPosition.y, worldPosition.z);
+    tint = 0.5f + 0.5f * normalize(tint);
+    
+    float3 finalcolor = basecolor * tint;
+    
+    
+    
+    return float4(finalcolor, 1.0f);
+
 }
 
 technique BasicColorDrawing
 {
-	pass P0
-	{
-		VertexShader = compile VS_SHADERMODEL MainVS();
-		PixelShader = compile PS_SHADERMODEL MainPS();
-	}
+    pass P0
+    {
+        VertexShader = compile VS_SHADERMODEL MainVS();
+        PixelShader = compile PS_SHADERMODEL MainPS();
+    }
 };
--- MultiRenders/Game1.cs
+++ MultiRenders/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Windows.Forms;
 
 namespace MultiRenders
 {
@@ -8,17 +9,33 @@ namespace MultiRenders
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Model _teapot;
+        private Effect _shader;
+        private Vector3 _teapotPosition;
+        private Matrix _world;
+        private Matrix _view;
+        private Matrix _projection;
+        private MouseState _previousMouseState;
+        private SpriteFont _font;
+        private Texture2D teapotTexture;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 600;
+            _graphics.PreferredBackBufferHeight = 600;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _teapotPosition = new Vector3(10, 0, 3); // Adjust the Z value as needed
+            _world = Matrix.CreateTranslation(_teapotPosition);
+            _view = Matrix.CreateLookAt(new Vector3(10, 0, 5), _teapotPosition, Vector3.Up);
+            _projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), _graphics.GraphicsDevice.Viewport.AspectRatio, 0.1f, 100f);
+
+            _previousMouseState = Mouse.GetState();
 
             base.Initialize();
         }
@@ -26,25 +43,69 @@ namespace MultiRenders
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            _teapot = Content.Load<Model>("Teapot"); 
+            _shader = Content.Load<Effect>("MyShader");
+            _font = Content.Load<SpriteFont>("Arial12");
+            teapotTexture = Content.Load<Texture2D>("Smiley2");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            MouseState currentMouseState = Mouse.GetState();
 
-            // TODO: Add your update logic here
+            if (currentMouseState != _previousMouseState)
+            {
+                _teapotPosition.X += (currentMouseState.X - _previousMouseState.X) * 0.01f;
+                _teapotPosition.Y -= (currentMouseState.Y - _previousMouseState.Y) * 0.01f;
+                _teapotPosition.Z -= (currentMouseState.ScrollWheelValue - _previousMouseState.ScrollWheelValue) * 0.01f;
+                _previousMouseState = currentMouseState;
+            }
+            _world = Matrix.CreateTranslation(_teapotPosition);
+
+            if (_graphics.PreferredBackBufferWidth != GraphicsDevice.Viewport.Width ||
+                _graphics.PreferredBackBufferHeight != GraphicsDevice.Viewport.Height)
+            {
+                _graphics.PreferredBackBufferWidth = GraphicsDevice.Viewport.Width;
+                _graphics.PreferredBackBufferHeight = GraphicsDevice.Viewport.Height;
+                _projection = Matrix.CreatePerspectiveFieldOfView(
+                    MathHelper.ToRadians(45),
+                    _graphics.GraphicsDevice.Viewport.AspectRatio,
+                    0.1f,
+                    100f
+                );
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            Matrix worldViewProjection = _world * _view * _projection;
+
+            _shader.CurrentTechnique = _shader.Techniques["BasicColorDrawing"];
+            _shader.Parameters["WorldViewProjection"].SetValue(worldViewProjection);
+            _shader.Parameters["World"].SetValue(_world);
+            _shader.Parameters["ViewProjection"].SetValue(_view * _projection);
+            _shader.Parameters["teapotTexture"].SetValue(teapotTexture);
+
+            // Apply the custom effect to each part of the teapot model
+            foreach (var mesh in _teapot.Meshes)
+            {
+                foreach (var part in mesh.MeshParts)
+                {
+                    part.Effect = _shader;
+                }
+            }
+
+            // Draw the teapot model
+            _teapot.Draw(_world, _view, _projection);
+
+            // Draw the teapot position
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_font, $"Teapot Position: {_teapotPosition}", new Vector2(10, 10), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
